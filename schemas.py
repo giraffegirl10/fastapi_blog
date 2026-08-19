533768b95dde5b22e5444d1cce769484e176d4a7
Heather Schoonmaker <heather.schoonmaker1@gmail.com>
2026-08-18 19:44:01 -0500

    Refactor code structure for improved readability and maintainability - Converted code to asynchronous

--- schemas.py
+++ schemas.py
@@ -17,8 +17,10 @@ class UserResponse(UserBase):
     image_file: str | None
     image_path: str
 
-
-
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None, min_length=1, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 class PostBase(BaseModel):
     title: str = Field(min_length=1, max_length=100)
@@ -27,6 +29,10 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     user_id: int #temporary
 
+class PostUpdate(BaseModel):
+    title: str | None= Field(default=None, min_length=1, max_length=100)
+    content: str | None = Field(default=None, min_length=1)
+
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
 
@@ -34,3 +40,4 @@ class PostResponse(PostBase):
     user_id: int
     date_posted: datetime
     author: UserResponse
+
